8346eaf009761daca8d2a7723b4ddb90ba6660ef
Caio Satel <caio_satel@hotmail.com>
2025-02-05 16:32:04 -0300

    feat: incluindo rotas de acesso na aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginHomeComponent } from './modules/login-home/login-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginHomeComponent},
+  { path: 'home', component: LoginHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
